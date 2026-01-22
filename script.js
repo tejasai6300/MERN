53ff6caf86f327da71ebae43f9316d4b0fe1010c
saiteja <tejsai3690@gmail.com>
2026-01-22 15:56:44 +0530

    javascript basics upto objects

---
+++ script.js
@@ -0,0 +1,125 @@
+// var a =10;
+// // // //var str="sai";
+// // // //var bool=true;
+// // // //var n=null;
+// // // //var un;
+// // // //var bi=123n;
+// // // //var sym=Symbol("li");
+
+
+// // // //console.log(typeof a);
+// // // //console.log(typeof str);
+// // // //console.log(typeof bool);
+// // // //console.log(typeof n);
+// // // //console.log(typeof un);
+// // // //console.log(typeof bi);
+// // // //console.log(typeof sym);
+
+
+// // // //relational operators
+
+
+// // // //var a=10;
+// // // //var b=20;
+// // // //console.log(a>b);
+// // // //console.log(a<b);
+// // // //console.log(a>=b);
+// // // //console.log(a<=b);
+// // // //var a=10;
+// // // //var str="10";
+// // // //console.log(a==b);
+// // // //console.log(a!=b);
+// // // //console.log(a===b);
+// // // //console.log(a!==b);
+
+// // // //logical operators
+// // // //var a=true;
+// // // //var b=false;
+// // // //console.log(a && b);
+// // // // console.log(a || b);
+// // // //console.log(!a);
+
+
+// // // //arrays
+// // // /*var arr=[10,20,30];
+// // // console.log(arr);
+// // // console.log(typeof arr);
+
+// // // //objects
+// // // var obj={name:"sai",age:20};
+// // // console.log(obj);
+// // // console.log(typeof obj);*/
+// // // //looping statements
+// // // for(var i=0;i<=10;i++){
+// // //     console.log(i);
+// // // }
+
+// // for(let i=10;i<10;i++){
+// //     console.log(i);
+
+// // }
+
+// // let mark =70;
+// // if(mark>=90) console.log("0grade");
+// // else if(mark>=80) console.log("A grade");
+// // else if(mark>=70) console.log("B Grade");
+// // else console.log("Fail");
+
+// //switch
+
+// let day=6;
+// switch(day){
+//     case 1:console.log("sunday");
+//     break;
+//     case 2:console.log("monday");
+//     break;
+//     case 3:console.log("tuesday");
+//     break;
+//     case 4:console.log("wed");
+//     break;
+    
+//     case 5:console.log("thurs");
+//   break;
+//     case 6:console.log("fri");
+// break;
+//     case 7:console.log("sat");
+//     break;
+//     default:console.log("invaild");
+// }
+
+
+
+//terminary operator
+// let i=8;
+// let result =  (i%2==0)?"even":"odd";
+// console.log(result);
+
+// let mark=30;
+// let result=(mark>=90)?"0 grade":(mark>=80)?"a grade":(mark>=70)?"b garde":"fail"
+
+// console.log(result);
+
+// function add(){
+//     console.log(10+20)
+// }
+// add();
+
+// var add=()=>{
+// console.log(10+20)
+// }
+// add();
+
+
+function add(a,b){
+    console.log(a+b);
+
+}
+add(30,40);
+var c;
+var add=(a,b)=>{
+    c=20;
+console.log(a+b+c);
+ }
+ //add(20,30);
+console.log(z);
+var z;
